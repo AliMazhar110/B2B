79d345d65c383d8f760a35baa7f25d28419f7bba
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-05-13 13:00:16 +0530

    Created database

--- src/module-info.java
+++ src/module-info.java
@@ -2,5 +2,6 @@ module B2B {
     requires javafx.fxml;
     requires javafx.controls;
     requires techsoftemail;
+    requires java.sql;
     opens sample;
 }
--- src/sample/Database.java
+++ src/sample/Database.java
@@ -3,5 +3,20 @@ package sample;
 import java.sql.*;
 
 public class Database {
-
+    public static void main(String[] args) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/world",
+                        "root", "AbcD123#");
+                System.out.println("Connection Successful");
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery("select Name from country where Population < 2000");
+                while (rs.next()) {
+                    System.out.println(rs.getString(1));
+                }
+                con.close();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
 }
