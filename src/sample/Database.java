287b72ba3d7b11a0a5ecec5ab3deff64a2cd3326
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-05-13 20:24:48 +0530

    Updated Database File

--- src/sample/Database.java
+++ src/sample/Database.java
@@ -3,20 +3,79 @@ package sample;
 import java.sql.*;
 
 public class Database {
-    public static void main(String[] args) {
-            try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/world",
-                        "root", "AbcD123#");
-                System.out.println("Connection Successful");
-                Statement st = con.createStatement();
-                ResultSet rs = st.executeQuery("select Name from country where Population < 2000");
-                while (rs.next()) {
-                    System.out.println(rs.getString(1));
-                }
-                con.close();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
+    private static final String url = "jdbc:mysql://localhost:3306/";
+    private static final String username = "root";
+    private static final String password = "AbcD123#";
+    private static final String className = "com.mysql.cj.jdbc.Driver";
+    private static String table;
+    private static String query;
+
+    private static void showMenu() {
+        System.out.println("\n1. Insert User"+
+                        "\n2. Show flights"+
+                        "\n3. Insert Booked flight"+
+                        "\n4. Cancel Booked Flight"+
+                        "\n5. Edit User's details"+
+                        "\n6. Show flight status"+
+                        "\n7. Timings of all the flights");
+    }
+
+    public static void access(int choice) {
+        table = "world";
+        try {
+            Class.forName(className);
+            Connection con = DriverManager.getConnection(url+table,
+                    username, password);
+            System.out.println("Connection Successful");
+
+            // Use of database
+            // 1. Input personal details and register and store the data
+            // 2. Login
+            // 3. Show flights
+            // 4. Input user's booked flight and data related to it
+            // 5. Cancel user's booked flight
+            // 6. Edit user's details
+            // 7. Show flight status
+            // 8. Timings of flights going from one place to another
+
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("select Name from country where Population < 2000");
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
             }
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
+    }
+
+    public static void insertUser(String name, String id, String passwd, String email, String mobile) {
+        table = "users";
+        try {
+            Class.forName(className);
+            Connection con = DriverManager.getConnection(url+table, username, password);
+            Statement st = con.createStatement();
+            query = "insert into "+ table +" values("+"'"+ name +"','"+ id +"','"+ passwd
+                    +"','"+ email +"','"+ mobile +"');";
+            ResultSet rs = st.executeQuery(query);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
